a108c33be1a6a80293b8e50adeb3dd59c4c720c4
Hao Dong <91730273+hdongly@users.noreply.github.com>
2024-03-21 19:22:40 +0800

    Modify

--- src/main/java/com/apple/producer/ProducerWithCallbackParallel.java
+++ src/main/java/com/apple/producer/ProducerWithCallbackParallel.java
@@ -1,46 +1,48 @@
 package com.apple.producer;
 
+
 import org.apache.kafka.clients.producer.*;
 import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.util.Properties;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
-public class ProducerWithCallback {
+public class ProducerWithCallbackParallel {
+
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         Properties props = new Properties();
-        //kafka集群，broker-list
+        // Kafka cluster, bootstrap servers
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "hadoop102:9092,hadoop103:9092,hadoop104:9092");
         props.put(ProducerConfig.ACKS_CONFIG, "all");
-        //重试次数（收不到ack时request.timeout.ms默认30秒就重发消息）
         props.put(ProducerConfig.RETRIES_CONFIG, 1);
-        //批次大小 16K
         props.put(ProducerConfig.BATCH_SIZE_CONFIG, 16384);
-        //等待时间（跟批次大小一起用）
         props.put(ProducerConfig.LINGER_MS_CONFIG, 1);
-        //RecordAccumulator缓冲区大小 32M
         props.put(ProducerConfig.BUFFER_MEMORY_CONFIG, 33554432);
-//        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
-//        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 
-        Producer<String, String> producer = new KafkaProducer<>(props);
+        // Number of producer threads
+        int numThreads = 5;
+        ExecutorService executor = Executors.newFixedThreadPool(numThreads);
 
         for (int i = 0; i < 100; i++) {
-            producer.send(new ProducerRecord<String, String>("first", Integer.toString(i), Integer.toString(i)), new Callback() {
-                //回调函数，该方法会在Producer收到ack时调用，为异步调用
-                @Override
-                public void onCompletion(RecordMetadata metadata, Exception exception) {
+            final int messageKey = i;
+            executor.submit(() -> {
+                Producer<String, String> producer = new KafkaProducer<>(props);
+                producer.send(new ProducerRecord<>("first", Integer.toString(messageKey), Integer.toString(messageKey)), (metadata, exception) -> {
                     if (exception == null) {
-                        System.out.println("success->" + metadata.offset());
+                        System.out.println("Message sent successfully -> Offset: " + metadata.offset());
                     } else {
                         exception.printStackTrace();
                     }
-                }
+                    producer.close();
+                });
             });
         }
 
-        producer.close();
+        // Shutdown executor after all tasks are completed
+        executor.shutdown();
     }
 }
--- src/main/java/com/apple/producer/ProducerWithoutCallback.java
+++ src/main/java/com/apple/producer/ProducerWithoutCallback.java
@@ -1,6 +1,7 @@
 package com.apple.producer;
 
 import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.util.Properties;
 import java.util.concurrent.ExecutionException;
@@ -9,7 +10,7 @@ public class ProducerWithoutCallback {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         Properties props = new Properties();
         //kafka集群，broker-list
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "hadoop102:9092");
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "hadoop102:9092,hadoop103:9092,hadoop104:9092");
         props.put(ProducerConfig.ACKS_CONFIG, "all");
         //重试次数（收不到ack时request.timeout.ms默认30秒就重发消息）
         props.put(ProducerConfig.RETRIES_CONFIG, 1);
@@ -19,8 +20,10 @@ public class ProducerWithoutCallback {
         props.put(ProducerConfig.LINGER_MS_CONFIG, 1);
         //RecordAccumulator缓冲区大小 32M
         props.put(ProducerConfig.BUFFER_MEMORY_CONFIG, 33554432);
-        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
-        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+//        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+//        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 
         Producer<String, String> producer = new KafkaProducer<>(props);
 
